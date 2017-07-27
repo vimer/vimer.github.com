00c5201422c18a43ec1f172007838702fafb2128
kissjs <emacs@vimer.org>
2017-07-27 18:20:50 +0800

    Site updated: 2017-07-27 18:20:50

--- js/main.js
+++ js/main.js
@@ -3,26 +3,28 @@ $(document).ready(function() {
   $('body').removeClass('no-js');
 
   $('a.blog-button').click(function() {
+    // If already in blog, return early without animate overlay panel again.
+    if (location.hash && location.hash == "#blog") return;
     if ($('.panel-cover').hasClass('panel-cover--collapsed')) return;
+    $('.main-post-list').removeClass('hidden');
     currentWidth = $('.panel-cover').width();
     if (currentWidth < 960) {
       $('.panel-cover').addClass('panel-cover--collapsed');
       $('.content-wrapper').addClass('animated slideInRight');
     } else {
       $('.panel-cover').css('max-width',currentWidth);
-      $('.panel-cover').animate({'max-width': '465px', 'width': '26%'}, 400, swing = 'swing', function() {} );
+      $('.panel-cover').animate({'max-width': '700px', 'width': '30%'}, 400, swing = 'swing', function() {
+		  //.......
+	  } );
     }
   });
 
   if (window.location.hash && window.location.hash == "#blog") {
     $('.panel-cover').addClass('panel-cover--collapsed');
+    $('.main-post-list').removeClass('hidden');
   }
 
   if (window.location.pathname.substring(0, 5) == "/tag/") {
-    $('.panel-cover').addClass('panel-cover--collapsed');
-  }
-  
-  if (window.location.pathname.substring(0, 6) == "/page/") {
     $('.panel-cover').addClass('panel-cover--collapsed');
   }
 
@@ -47,26 +49,19 @@ $(document).ready(function() {
 
       $('.navigation-wrapper').toggleClass('animated bounceInDown animated bounceOutUp');
     }
+    
     $('.btn-mobile-menu__icon').toggleClass('fa fa-list fa fa-angle-up animated fadeIn');
   });
-
-  $("article.post-container--single a[href^=http]").attr("target", "_blank");
-  $("article.post-container--single a[href^=mailto]").attr("target", "_blank");
-  
-  
-  
-  $(function(){
-		
-		$('.img-click').click(function(){
-            document.getElementById("img-content").src=$(this).attr("data");
-			$('.img-background').fadeIn(200);
-			$('.img-border').fadeIn(400);
-            
-		});
-		$('.img-background').click(function(){
-			$('.img-background').fadeOut(200);
-			$('.img-border').fadeOut(200);
-		});
-        
-	});
 });
+
+setShareData("http://vimer.me/images/show_favicon.png");
+function setShareData(imgUrl) {
+   if (imgUrl) {
+      var img = new Image();
+      img.src = imgUrl;
+      img.style.position = 'absolute';
+      img.style.left = '-1000px';
+      img.style.top = '-1000px';
+      document.body.insertBefore(img, document.body.firstChild);
+   };
+};
